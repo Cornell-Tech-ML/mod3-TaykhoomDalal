bd7148fcdb37f82e91abd3a3dcd979b41c6b68d0
Taykhoom Dalal <tid4007@med.cornell.edu>
2024-11-15 17:43:22 -0500

    removed extra tensor import

--- minitorch/__init__.py
+++ minitorch/__init__.py
@@ -12,7 +12,6 @@ from .autodiff import *  # noqa: F401,F403
 from .module import *  # noqa: F401,F403
 from .module import *  # noqa: F401,F403
 from .autodiff import *  # noqa: F401,F403
-from .tensor import *  # noqa: F401,F403
 from .datasets import *  # noqa: F401,F403
 from .testing import *  # noqa: F401,F403
 from .optim import *  # noqa: F401,F403
